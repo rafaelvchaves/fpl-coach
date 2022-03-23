4d45704b3b4655fc69817c45b17c5b47c72231c9
Rafael Chaves <rafaelvchaves@gmail.com>
2022-03-22 23:11:15 -0400

    Renamed script folder and updated fixtures + gw data

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -2,7 +2,8 @@ USE fplcoach_db;
 
 CREATE TABLE IF NOT EXISTS teams(
   id INT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  understat_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS players(
@@ -33,30 +34,32 @@ CREATE TABLE IF NOT EXISTS fixtures(
   FOREIGN KEY away_team REFERENCES teams(name)
 );
 
-CREATE TABLE IF NOT EXISTS team_gws(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  team_id INT,
-  gameweek INT NOT NULL,
-  offensive_rating FLOAT,
-  defensive_rating FLOAT,
-  FOREIGN KEY (team_id) REFERENCES teams(id)
-);
+-- CREATE TABLE IF NOT EXISTS team_gws(
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   team_id INT,
+--   gameweek INT NOT NULL,
+--   offensive_rating FLOAT,
+--   defensive_rating FLOAT,
+--   FOREIGN KEY (team_id) REFERENCES teams(id)
+-- );
 
 CREATE TABLE IF NOT EXISTS player_gws(
-  id INT AUTO_INCREMENT PRIMARY KEY,
   player_id INT,
   gameweek INT,
   fixture_id INT,
+  team_id INT,
   opponent_id INT,
-  price FLOAT,
   minutes_played INT,
   npxg FLOAT,
   xa FLOAT,
   bonus_points INT,
   total_points INT,
+  price FLOAT,
   FOREIGN KEY (player_id) REFERENCES players(id),
+  FOREIGN KEY (fixture_id) REFERENCES fixtures(id),
+  FOREIGN KEY (team_id) REFERENCES teams(id),
   FOREIGN KEY (opponent_id) REFERENCES teams(id),
-  FOREIGN KEY (fixture_id) REFERENCES fixtures(id)
+  PRIMARY KEY (player_id, fixture_id)
 );
 
 -- CREATE TABLE IF NOT EXISTS manager_gws(
