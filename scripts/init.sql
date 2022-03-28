ab1a7ba4f41eaf77fd0caaf5324b98a887be0348
Rafael Chaves <rafaelvchaves@gmail.com>
2022-03-28 11:49:52 -0400

    Updated SQL init script to have correct column names

--- scripts/init.sql
+++ scripts/init.sql
@@ -12,8 +12,8 @@ DROP TABLE IF EXISTS teams;
 
 
 CREATE TABLE IF NOT EXISTS teams(
-  id INT PRIMARY KEY,
-  fpl_name VARCHAR(255) NOT NULL,
+  fpl_id INT NOT NULL,
+  fpl_name VARCHAR(255) PRIMARY KEY,
   understat_name VARCHAR(255) NOT NULL
 );
 
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS players(
   understat_id INT NOT NULL,
   fpl_name VARCHAR(255) NOT NULL,
   position CHAR NOT NULL,
-  team_name INT NOT NULL,
-  FOREIGN KEY (team_name) REFERENCES teams(id)
+  team_name VARCHAR(255) NOT NULL,
+  FOREIGN KEY (team_name) REFERENCES teams(fpl_name)
 );
 
 CREATE TABLE IF NOT EXISTS managers(
@@ -35,41 +35,43 @@ CREATE TABLE IF NOT EXISTS fixtures(
   id INT PRIMARY KEY,
   gameweek INT,
   kickoff_date DATETIME,
-  home_team INT NOT NULL,
+  home_team VARCHAR(255) NOT NULL,
   home_score INT,
   away_score INT,
-  away_team INT NOT NULL,
-  home_xg FLOAT,
-  away_xg FLOAT,
-  FOREIGN KEY (home_team) REFERENCES teams(id),
-  FOREIGN KEY (away_team) REFERENCES teams(id)
+  away_team VARCHAR(255) NOT NULL,
+  home_xG FLOAT,
+  away_xG FLOAT,
+  FOREIGN KEY (home_team) REFERENCES teams(fpl_name),
+  FOREIGN KEY (away_team) REFERENCES teams(fpl_name)
 );
 
 CREATE TABLE IF NOT EXISTS team_gws(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  team_id INT,
-  gameweek INT NOT NULL,
-  offensive_rating FLOAT,
-  defensive_rating FLOAT,
-  FOREIGN KEY (team_id) REFERENCES teams(id)
+  gameweek INT,
+  fixture_id INT NOT NULL,
+  team VARCHAR(255) NOT NULL,
+  opponent VARCHAR(255) NOT NULL,
+  home BOOLEAN NOT NULL,
+  team_xG FLOAT,
+  team_xGA FLOAT,
+  FOREIGN KEY (team) REFERENCES teams(fpl_name),
+  FOREIGN KEY (opponent) REFERENCES teams(fpl_name),
+  PRIMARY KEY (gameweek, team, opponent)
 );
 
 CREATE TABLE IF NOT EXISTS player_gws(
   player_id INT,
-  gameweek INT,
   fixture_id INT,
-  team_id INT,
-  opponent_id INT,
+   -- not necessarily the same as team name from players table - players can move between EPL teams --
+  team VARCHAR(255),
   minutes_played INT,
-  npxg FLOAT,
-  xa FLOAT,
-  bonus_points INT,
+  npxG FLOAT,
+  xA FLOAT,
+  bonus INT,
   total_points INT,
   price FLOAT,
   FOREIGN KEY (player_id) REFERENCES players(fpl_id),
   FOREIGN KEY (fixture_id) REFERENCES fixtures(id),
-  FOREIGN KEY (team_id) REFERENCES teams(id),
-  FOREIGN KEY (opponent_id) REFERENCES teams(id),
+  FOREIGN KEY (team) REFERENCES teams(fpl_name),
   PRIMARY KEY (player_id, fixture_id)
 );
 
