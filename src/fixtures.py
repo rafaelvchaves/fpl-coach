aa1600b483e2544e2342dd78da0ddfc5ea7f9ab0
Rafael Chaves <rafaelvchaves@gmail.com>
2022-04-09 16:48:07 -0400

    Reworked fixtures to come entirely from Understat + added team gw populator

--- src/fixtures.py
+++ src/fixtures.py
@@ -1,105 +1,78 @@
 import aiohttp
 import asyncio
-import csv
-import datetime
-import json
 import requests
-from constants import CURRENT_SEASON, FIXTURES_FILE, FPL_FIXTURES_URL
-from dateutil import parser
-from functools import cmp_to_key
-from teams import id_to_name_map
-from teams import understat_to_fpl_map
-from typing import Optional
+from constants import CURRENT_SEASON, FPL_FIXTURES_URL
+from teams import id_to_name_map, understat_to_fpl_map
 from understat import Understat
-from utils import parse_date, compare_by_index
+from utils import *
 from db import MySQLManager
 
-team_map = id_to_name_map()
-name_map = understat_to_fpl_map()
-
-async def get_team_understat_data():
-    """Retrieves Understat data from all teams in the current season.
-
-    Returns:
-        A dictionary with dates as keys, which map to a dictionary of teams that
-        played that day and their corresponding statistics in that fixture. For
-        example,
-        {
-            "2021-08-13": {
-                "Arsenal": {
-                    "xG": 1.024,
-                    "xGA": 1.888
-                },
-                ...
-            },
-            ....
-        }
-    """
-    async with aiohttp.ClientSession() as session:
-        fixture_map = {}
-        understat = Understat(session)
-        teams = await understat.get_teams("epl", CURRENT_SEASON[:-3])
-        for team in teams:
-            for game in team["history"]:
-                date = parse_date(game["date"])
-                team_name = name_map[team["title"]]
-                if date not in fixture_map:
-                    fixture_map[date] = {}
-                if team_name not in fixture_map[date]:
-                    fixture_map[date][team_name] = {
-                        "xG": float(game["xG"]),
-                        "xGA": float(game["xGA"])
-                    }
-        return fixture_map
-
-understat_team_data = asyncio.run(get_team_understat_data())
-
-def get_xG_data(fixture: dict, date : Optional[str]):
-    """Get the xG of the home and away team in a certain fixture.
-
-    Returns:
-        A tuple (hxG, axG), where hxG is the home team's xG and axG is the
-        away team's xG for that fixture, if this fixture has occurred.
-        (None, None) otherwise.
-    """
-    today = datetime.date.today().strftime("%Y-%m-%d")
-    if date is not None and date < today:
-        match_data = understat_team_data[date]
-        home_team = team_map[fixture["team_h"]]
-        away_team = team_map[fixture["team_a"]]
-        if home_team in match_data:
-            home_xG = match_data[home_team]["xG"]
-            away_xG = match_data[away_team]["xG"]
-            return round(home_xG, 3), round(away_xG, 3)
-    return None, None
+id_to_name = id_to_name_map()
+ustat_to_fpl = understat_to_fpl_map()
 
 
-def get_fixture_info(fixture : dict) -> dict:
-    """Extract data from a fixture json."""
-    date = parse_date(fixture["kickoff_time"])
-    home_xG, away_xG = get_xG_data(fixture, date, understat_team_data)
-    return {
-        "id": fixture["id"],
-        "gameweek": fixture["event"],
-        "kickoff_date": date,
-        "home_team": team_map[fixture["team_h"]],
-        "home_score": fixture["team_h_score"],
-        "away_score": fixture["team_a_score"],
-        "away_team": team_map[fixture["team_a"]],
-        "home_xG": home_xG,
-        "away_xG": away_xG
+def add_fixture(fixture_map, fixture):
+    home_team = ustat_to_fpl[fixture["h"]["title"]]
+    away_team = ustat_to_fpl[fixture["a"]["title"]]
+    if not home_team in fixture_map:
+        fixture_map[home_team] = {}
+    fixture_map[home_team][away_team] = {
+        "fpl_id": None,
+        "understat_id": cast_int_safe(fixture["id"]),
+        "gameweek": None,
+        "kickoff_date": parse_date(fixture["datetime"]),
+        "home_team": home_team,
+        "home_score": cast_int_safe(fixture["goals"]["h"]),
+        "away_score": cast_int_safe(fixture["goals"]["a"]),
+        "away_team": away_team,
+        "home_xG": cast_float_safe(fixture["xG"]["h"]),
+        "away_xG": cast_float_safe(fixture["xG"]["a"])
     }
 
+
+async def get_understat_fixture_data():
+    async with aiohttp.ClientSession() as session:
+        understat = Understat(session)
+        old_fixtures = await understat.get_league_results("epl", CURRENT_SEASON[:-3])
+        new_fixtures = await understat.get_league_fixtures("epl", CURRENT_SEASON[:-3])
+        old_fixtures.extend(new_fixtures)
+        fixture_map = {}
+        for fixture in old_fixtures:
+            add_fixture(fixture_map, fixture)
+        return fixture_map
+
+
+def create_team_gw_row(fixture, home):
+    return {
+        "gameweek": fixture["gameweek"],
+        "fixture_id": fixture["fpl_id"],
+        "team": fixture["home_team"] if home else fixture["away_team"],
+        "opponent": fixture["away_team"] if home else fixture["home_team"],
+        "home": home,
+        "team_xG": fixture["home_xG"] if home else fixture["away_xG"],
+        "team_xGA": fixture["away_xG"] if home else fixture["home_xG"]
+    }
+
+
+def get_team_gws(fixtures):
+    rows = []
+    for fixture in fixtures:
+        rows.append(create_team_gw_row(fixture, True))
+        rows.append(create_team_gw_row(fixture, False))
+    return rows
+
+
 def get_fixtures():
-    """Writes fixture data to csv file.
-    
-    First, all of the fixtures are retrieved from the FPL API, and then each
-    fixture is populated with the corresponding Understat xG data, if possible.
-    """
-    print("Getting fixtures...")
-    fixtures_json = requests.get(FPL_FIXTURES_URL).json()
-    rows = [get_fixture_info(fj, understat_team_data) for fj in fixtures_json]
-    print("Done")
+    fpl_fixtures = requests.get(FPL_FIXTURES_URL).json()
+    understat_fixture_data = asyncio.run(get_understat_fixture_data())
+    rows = []
+    for fixture in fpl_fixtures:
+        home_team = id_to_name[fixture["team_h"]]
+        away_team = id_to_name[fixture["team_a"]]
+        row = understat_fixture_data[home_team][away_team]
+        row["fpl_id"] = fixture["id"]
+        row["gameweek"] = fixture["event"]
+        rows.append(row)
     return rows
 
 
@@ -107,3 +80,5 @@ if __name__ == "__main__":
     db = MySQLManager()
     fixtures = get_fixtures()
     db.writerows(fixtures, "fixtures")
+    team_gws = get_team_gws(fixtures)
+    db.writerows(team_gws, "team_gws")
