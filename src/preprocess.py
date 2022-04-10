ddabdf9f16145eb342d99918b7c8ad49933348ca
Rafael Chaves <rafaelvchaves@gmail.com>
2022-04-10 11:15:20 -0400

    Changed preprocess to read from SQL rather than csv file

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,58 +1,44 @@
-import datetime
-import json
-import os
-import numpy as np
+import mysql.connector
 import pandas as pd
-from constants import FIXTURES_FILE, GW_HISTORY_FILE, MERGED_DATA_FILE
+from constants import GW_HISTORY_FILE
+from sqlalchemy import create_engine
 
-
-def merge():
-  gdf = pd.read_csv(GW_HISTORY_FILE)
-  fdf = pd.read_csv(FIXTURES_FILE)
-
-  # subsample columns
-  fdf = fdf[["fixture_id", "home_team", "away_team"]]
-
-  # merge with fixtures
-  df = pd.merge(gdf, fdf, on="fixture_id", how="outer").drop(
-      columns=["fixture_id"])
-
-  # set team column
-  df.loc[df["home"], "team"] = df["home_team"]
-  df.loc[~df["home"], "team"] = df["away_team"]
-
-  # set opposition column
-  df.loc[df["home"], "opponent"] = df["away_team"]
-  df.loc[~df["home"], "opponent"] = df["home_team"]
-  df = df.drop(columns=["home_team", "away_team", "home"])
-  df.to_csv(MERGED_DATA_FILE, index=False)
-  return df
-
-
-def average_stat(df, stat, by="name", over_all_games=False, fillna=True, drop_old=False, window=4):
+def average_stat(df, stat, by="player_name", over_all_games=False, fillna=True, drop_old=False, window=4):
     tdf = df.copy()
     avg_name = "a" + stat
     for val in df[by].unique():
         val_rows = df[by] == val
-        rows_to_get = val_rows & (over_all_games | (df["minutes"] > 0) | ~df["completed"])
+        rows_to_get = val_rows & (over_all_games | (
+            df["minutes_played"] > 0) | ~df["completed"])
         rows_to_set = val_rows & df["completed"]
-        rolling_avg = df.loc[rows_to_get, stat].rolling(window=window, closed="left", min_periods=1).mean()
+        rolling_avg = df.loc[rows_to_get, stat].rolling(
+            window=window, closed="left", min_periods=1).mean()
         tdf.loc[rows_to_set, avg_name] = rolling_avg
         if fillna:
-            tdf.loc[val_rows, avg_name] = tdf.loc[val_rows, avg_name].fillna(method="ffill")
+            tdf.loc[val_rows, avg_name] = tdf.loc[val_rows,
+                                                  avg_name].fillna(method="ffill")
     if drop_old:
         tdf.drop(columns=stat, inplace=True)
     return tdf
 
-def get_data():
-  df = pd.read_csv(MERGED_DATA_FILE)
-  df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
-  df["completed"] = df["date"] < datetime.datetime.today()
-  df = average_stat(df, "npxG")
-  df = average_stat(df, "xA")
-  df = average_stat(df, "bonus")
-  df = average_stat(df, "minutes", over_all_games=True)
-  return df
+
+def preprocess():
+    con = create_engine('mysql://root:password@localhost/fplcoachdb')
+    query = """SELECT player_name, gameweek, player_gws.team AS team,
+      completed, minutes_played, npxG, xA, bonus, total_points, price,
+      team_xG, team_xGA
+      FROM
+        player_gws INNER JOIN team_gws
+        ON player_gws.fixture_id = team_gws.fixture_id
+        AND player_gws.team = team_gws.team
+      ORDER BY kickoff_date"""
+    df = pd.read_sql(query, con)
+    df = average_stat(df, "npxG")
+    df = average_stat(df, "xA")
+    df = average_stat(df, "bonus")
+    df = average_stat(df, "minutes_played", over_all_games=True)
+    df.to_csv(GW_HISTORY_FILE, index=False)
+
 
 if __name__ == "__main__":
-  merge()
+    preprocess()
