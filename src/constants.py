71a92525c4d971194748d14431bdd1d1ed3806f2
Rafael Chaves <rafaelvchaves@gmail.com>
2022-04-10 11:12:26 -0400

    Edited constants

--- src/constants.py
+++ src/constants.py
@@ -9,7 +9,6 @@ FPL_FIXTURES_URL = "https://fantasy.premierleague.com/api/fixtures/"
 DATA_DIR = "../data"
 FIXTURES_FILE = join(DATA_DIR, "fixtures.csv")
 GW_HISTORY_FILE = join(DATA_DIR, "gw_history.csv")
-MERGED_DATA_FILE = join(DATA_DIR, "merged_data.csv")
 PLAYERS_FILE = join(DATA_DIR, "players_{}.json".format(CURRENT_SEASON))
 TEAMS_FILE = join(DATA_DIR, "teams_{}.json".format(CURRENT_SEASON))
 UNDERSTAT_PLAYER_FILE = join(DATA_DIR, "understat_player_data.json")
