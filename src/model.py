0b7d1f153527f6ad379d66a36e04bc546550dc4a
Rafael Chaves <rafaelvchaves@gmail.com>
2022-04-24 15:26:22 -0400

    Tweaks to model + added initial evaluation function in model.py

--- src/model.py
+++ src/model.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
-from constants import *
+from constants import (
+    GW_HISTORY_FILE,
+    RUNS_FILE,
+    NPXG_ALPHA,
+    XA_ALPHA,
+    BONUS_ALPHA,
+    MINUTES_ALPHA
+)
 from scipy.stats import poisson
 from db import MySQLManager
-from utils import *
+from preprocess import preprocess
+from utils import from_json, to_json, get_current_gw
 from typing import *
 
 
@@ -22,70 +30,91 @@ def expected(pmf: Callable[[int], float], value: int, max_value: int) -> float:
     """
     xp = 0
     for x in range(1, max_value + 1):
-        xp += value * x * pmf(x)
-    return xp
+        xp += x * pmf(x)
+    return value * xp
 
 
-def predict_points(position, apxG, apxA, amins, abonus, atxG, atxGA, aoxG, aoxGA, proj_score, oproj_score):
-    position_params = from_json("../data/params.json")[position]
-    goal_value = position_params["goal_value"]
-    assist_value = position_params["assist_value"]
-    clean_sheet_value = position_params["cs_value"]
-    tgcv = position_params["concede_value"]
-    xp = []
-    attack_multiplier = proj_score / atxG
-    def goal_pmf(x): return poisson(attack_multiplier * apxG).pmf(x)
-    def assist_pmf(x): return poisson(attack_multiplier * apxA).pmf(x)
-    def bonus_pmf(x): return poisson(abonus).pmf(x)
-    def cs_pmf(x): return poisson(oproj_score).pmf(0)
-    def concede_pmf(x): return poisson(atxGA).pmf(2 * x)
-
-    # can cap goals/assists at 4 since probability beyond that should be negligible anyway
-    xp.append(expected(goal_pmf, goal_value, 4))
-    xp.append(expected(assist_pmf, assist_value, 4))
-    xp.append(expected(bonus_pmf, 1, 3))
-    xp.append(expected(cs_pmf, clean_sheet_value, 1))
-    xp.append(expected(concede_pmf, tgcv, 4))
-    xp.append(2)
-    mins_multiplier = min(1, round(amins / 60, 1))
-    xp.append(mins_multiplier * np.sum(xp))
-    return np.round(xp, 3)
+params_json = from_json("../data/params.json")
 
 
 def predict(row):
-    return predict_points(
-        row["position"],
-        row["avg_npxG"],
-        row["avg_xA"],
-        row["avg_minutes"],
-        row["avg_bonus"],
-        row["avg_team_xG"],
-        row["avg_team_xGA"],
-        row["avg_opponent_xG"],
-        row["avg_opponent_xGA"],
-        row["proj_score"],
-        row["opponent_proj_score"]
-    )
+    xp = []
+    stat_values = params_json[row["position"]]["stat_values"]
+    attack_multiplier = row["proj_score"] / row["avg_team_xG"]
+    def goal_pmf(x): return poisson(attack_multiplier * row["avg_npxG"]).pmf(x)
+    def assist_pmf(x): return poisson(attack_multiplier * row["avg_xA"]).pmf(x)
+    def bonus_pmf(x): return poisson(row["avg_bonus"]).pmf(x)
+    def cs_pmf(x): return poisson(row["opponent_proj_score"]).pmf(0)
+    def concede_pmf(x): return poisson(row["opponent_proj_score"]).pmf(2 * x)
 
-# TODO: add function that evaluates model and prints metrics when this script
-# is run
+    # can cap goals/assists at 4 since probability beyond that should be negligible anyway
+    xp.append(expected(goal_pmf, stat_values[0], 4))
+    xp.append(expected(assist_pmf, stat_values[1], 4))
+    xp.append(expected(cs_pmf, stat_values[2], 1))
+    xp.append(expected(bonus_pmf, stat_values[3], 3))
+    xp.append(expected(concede_pmf, stat_values[4], 4))
+    mins_multiplier = min(1, round(row["avg_minutes"] / 60, 1))
+    # Add 2 for base minute points
+    xp.append(mins_multiplier * (np.sum(xp) + 2))
+    return np.round(xp, 3)
 
 
-# will need to do hyperparameter tuning for each EMA alpha, probably collecting
-# more data to break up point total into points from goals, clean sheets, etc.
+predicted_cols = [
+    "goal_xP", "assist_xP", "cs_xP", "bonus_xP", "concede_xP", "xP"
+]
+
+point_cols = [
+    "goal_points", "assist_points", "cs_points", "bonus", "concede_points"
+]
+
+stat_cols = [
+    "goals_scored", "assists", "clean_sheets", "bonus", "goals_conceded"
+]
+
+
+def calculate_points(row):
+    stat_values = params_json[row["position"]]["stat_values"]
+    return row[stat_cols] * stat_values
+
+
+def evaluate(rows):
+    known_point_totals = rows[rows["gameweek"] < get_current_gw()].copy()
+    known_point_totals[point_cols] = known_point_totals.apply(
+        calculate_points, axis=1, result_type="expand")
+    all_point_cols = point_cols + ["total_points"]
+    known_point_totals["concede_points"] = - \
+        known_point_totals["goals_conceded"] // 2
+    actual_points = known_point_totals[all_point_cols].to_numpy()
+    predicted_points = known_point_totals[predicted_cols].to_numpy()
+    diff_sq = np.power(actual_points - predicted_points, 2)
+    rmse = np.round(np.sqrt(np.sum(diff_sq, axis=0)), 1)
+    print("RMSE: ", rmse)
+    runs = from_json(RUNS_FILE)
+    runs.append({
+        "npxG_alpha": NPXG_ALPHA,
+        "xA_alpha": XA_ALPHA,
+        "bonus_alpha": BONUS_ALPHA,
+        "minutes_alpha": MINUTES_ALPHA,
+        "rmse": rmse.tolist()
+    })
+    to_json(RUNS_FILE, runs)
+
+
 if __name__ == "__main__":
+    preprocess()
     df = pd.read_csv(GW_HISTORY_FILE)
-    stats = ["goal", "assist", "bonus",
-             "cs", "concede", "minutes"]
-    predicted_cols = [stat + "_xP" for stat in stats]
-    predicted_cols.append("xP")
-    rows = df.copy()
-    rows[predicted_cols] = rows.apply(predict, axis=1, result_type="expand")
+    rows = df[df["avg_minutes"] > 0].copy()
+    rows[predicted_cols] = rows.apply(
+        predict, axis=1, result_type="expand")
+    zero_rows = df[df["avg_minutes"] == 0].copy()
+    zero_rows[predicted_cols] = 0
+    rows.to_csv("../data/predictions.csv", index=False)
+    evaluate(rows)
+    all_rows = pd.concat([rows, zero_rows])
     db = MySQLManager()
-    for _, r in rows.iterrows():
+    for _, row in all_rows.iterrows():
         db.update_row(
             "player_gws_predicted",
-            {"player_name": r["player_name"], "fixture_id": r["fixture_id"]},
-            {col: r[col] for col in predicted_cols}
+            {"player_id": row["player_id"], "fixture_id": row["fixture_id"]},
+            {col: row[col] for col in predicted_cols}
         )
-    rows.to_csv("../data/predicted_final.csv", index=False)
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,19 +1,41 @@
 import numpy as np
-from constants import GW_HISTORY_FILE
+from constants import GW_HISTORY_FILE, NPXG_ALPHA, XA_ALPHA, BONUS_ALPHA, MINUTES_ALPHA
 from db import MySQLManager
 from utils import *
+from teams import get_fpl_teams
 
 
 def compute_emas(df, stats, alpha):
     """Computes an EMA for each of the specified stats."""
     dfc = df.copy()
     for stat in stats:
-        avg_stat = "avg_" + stat
+        avg_stat = f"avg_{stat}"
         for player in df["player_name"].unique():
-            row = df[df["player_name"] == player]
-            dfc.loc[df["player_name"] == player,
-                    avg_stat] = get_ema(row[stat], alpha)
+            is_player = df["player_name"] == player
+            row = df[is_player]
+            dfc.loc[is_player, avg_stat] = get_ema(row[stat], alpha)
     return dfc
+
+def compute_team_xg_averages(db: MySQLManager) -> None:
+    """Averages team xG data and writes to the team_gws table.
+
+    Averages are calculated with an exponential moving average (EMA)."""
+    teams = get_fpl_teams()
+    for team in teams:
+        team_name = team["fpl_name"]
+        query = """SELECT * FROM team_gws
+        WHERE gameweek IS NOT NULL
+        ORDER BY kickoff_date"""
+        df = db.get_df(query)
+        team_rows = df[df["team"] == team_name].reset_index()
+        team_xg = get_ema(team_rows["team_xG"], NPXG_ALPHA)
+        team_xga = get_ema(team_rows["team_xGA"])
+        for i, team_row in team_rows.iterrows():
+            db.update_row(
+                "team_gws",
+                {"fixture_id": team_row["fixture_id"], "team": team_name},
+                {"avg_team_xG": team_xg[i], "avg_team_xGA": team_xg[i]}
+            )
 
 
 def preprocess():
@@ -21,12 +43,15 @@ def preprocess():
 
     Joins the player_gws table with team_gws table and computes stat averages."""
     db = MySQLManager()
-    query = open("../scripts/merge.sql").read()
+    compute_team_xg_averages(db)
+    with open("../scripts/merge.sql") as f:
+        query = f.read()
     df = db.get_df(query)
     df = df.sort_values(by="kickoff_date")
-    df = compute_emas(df, ["npxG", "xA", "bonus"],
-                      0.3)  # weight bonus even less?
-    df = compute_emas(df, ["minutes"], 0.5)
+    df = compute_emas(df, ["npxG"], NPXG_ALPHA)
+    df = compute_emas(df, ["xA"], XA_ALPHA)
+    df = compute_emas(df, ["bonus"], BONUS_ALPHA)
+    df = compute_emas(df, ["minutes"], MINUTES_ALPHA)
     df.to_csv(GW_HISTORY_FILE, index=False)
 
 
